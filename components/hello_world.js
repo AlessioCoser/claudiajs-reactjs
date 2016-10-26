22601579da631db8a3fde2b02a988c2e6f094c64
alessiocoser <alessio.coser@xpeppers.com>
2016-10-26 18:00:44 +0200

    extract hello_world component

---
+++ components/hello_world.js
@@ -0,0 +1,9 @@
+var React = require('react')
+
+var DOM = React.DOM
+
+module.exports = React.createFactory(React.createClass({
+ render: function() {
+   return DOM.span(null, "hello world");
+ }
+}));
--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 var ApiBuilder = require('claudia-api-builder')
 var React = require('react')
 var ReactDOMServer = require('react-dom/server');
+var helloWorld = require('./components/hello_world')
 
 var api = new ApiBuilder()
 var DOM = React.DOM
-
-var helloWorld = React.createFactory(React.createClass({
- render: function() {
-   return DOM.span(null, "hello world");
- }
-}));
 
 module.exports = api
 
