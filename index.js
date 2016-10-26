6dc77d7245563d1845ad583f97d237c9c49ffadc
alessiocoser <alessio.coser@xpeppers.com>
2016-10-26 15:11:39 +0200

    add first simple component

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var ApiBuilder = require('claudia-api-builder')
+var React = require('react')
+var ReactDOMServer = require('react-dom/server');
 
 var api = new ApiBuilder()
+var DOM = React.DOM
+
+var helloWorld = React.createFactory(React.createClass({
+ render: function() {
+   return DOM.span(null, "hello world");
+ }
+}));
 
 module.exports = api
 
 api.get('/', function () {
-  var response = 'hello world'
+  var response = ReactDOMServer.renderToStaticMarkup(DOM.body(null, helloWorld(null)))
   var statusCode = 200
   var contentType = 'text/html'
 
